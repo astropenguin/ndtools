9525f4345bcf29f8a3dcb933764dfd27532ca86c
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2026-02-04 10:30:44 +0000

    #31 Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,4 @@
-# project information
-author = "Akio Taniguchi"
-copyright = "2025 Akio Taniguchi"
-
-
-# general configuration
-add_module_names = False
-autodoc_member_order = "bysource"
-autodoc_typehints = "both"
-autodoc_typehints_format = "short"
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+copyright = "2025-2026 Akio Taniguchi"
 extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",
@@ -16,13 +6,18 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
 ]
-myst_heading_anchors = 3
-templates_path = ["_templates"]
-
-
-# options for HTML output
+html_static_path = ["_static"]
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
     "github_url": "https://github.com/astropenguin/ndtools",
     "logo": {"text": "ndtools"},
+    "navbar_end": [
+        "version-switcher",
+        "theme-switcher",
+        "navbar-icon-links",
+    ],
+    "switcher": {
+        "json_url": "https://astropenguin.github.io/ndtools/_static/switcher.json",
+        "version_match": "1.1.0",
+    },
 }
